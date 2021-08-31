75a8362e1cc2e4e5231b79f6e5e968689d691d07
Ted <Ted31539@gmail.com>
2021-08-31 16:04:10 +0800

    Update 2021-08-31T08:04:05.201Z

---
+++ assets/js/all.js
@@ -0,0 +1,20 @@
+"use strict";
+
+$(function () {
+  console.log('Hello Bootstrap5');
+}); // Initialize Swiper
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
+}); // Initialize Swiper AOS
+
+AOS.init({
+  // Global settings:  
+  once: true // whether animation should happen only once - while scrolling down
+
+});
+//# sourceMappingURL=all.js.map
