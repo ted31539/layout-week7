66ba7b8c89d63cb5eb234dbbb29f03e5e52eee2f
Ted <Ted31539@gmail.com>
2021-08-31 17:47:35 +0800

    animate

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -12,5 +12,5 @@ var swiper = new Swiper(".mySwiper", {
 // Initialize Swiper AOS
 AOS.init({
   // Global settings:  
-  once: true, // whether animation should happen only once - while scrolling down
+  once: false, // whether animation should happen only once - while scrolling down
 });
